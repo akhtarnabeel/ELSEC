9ce2c309ef7de78b22d98c6c3df2663576f9c4b5
akhtarnabeel <akhtarnabeel@users.noreply.github.com>
2018-01-23 20:17:29 -0500

    More figures

---
+++ Control/AttackAnalyzer/AttackAnalyzer.py
@@ -0,0 +1,114 @@
+__author__ = 'Nabeel'
+
+import os, time, argparse
+
+def main():
+    pauseTime = 0.2
+
+    # set the argument parer to take input file as input
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--filepath", help="path to snort attacker file")
+    args = parser.parse_args()
+
+    if args.filepath == None:
+        print 'Please enter file path to snort alert file'
+        parser.print_help()
+        exit(0)
+
+    inputFile = args.filepath
+
+    # print the name of input file
+    print inputFile
+
+    # get types of attacks and save them in array
+    exists = os.path.isfile('attackType.config')
+    if exists == 1:
+        # add to array
+        f1 = open('attackType.config', 'r')
+        attackType = f1.read()
+        f1.close()
+        attackType_list = attackType.split('\n')
+    else:
+        print 'File to defile attacks does not exist!'
+        return
+
+    while(True):
+        try:
+
+            # check if file exists, otherwise wait for sometime and try again!
+            while (True):
+                ans = os.path.isfile(inputFile)
+
+                # if file found, then proceed. Otherwise keep waiting
+                if ans == 1:
+                    break
+                else:
+                    print 'Snort alert file does not exists at ', inputFile, '\nChecking again...'
+                    time.sleep(pauseTime)
+
+            snort_alert_f = open(inputFile, 'r')
+            snort_alert = snort_alert_f.read()
+            snort_alert_f.close()
+
+            # read the file and if there is anything, add to alert.txt file
+            attacker_list = []
+            alerts = snort_alert.split('\n')
+
+            j = 0
+            while j < len(attackType_list):
+                # ping attack
+                if attackType_list[j] == 'PING':
+                    attackString = '-> 10.10.1.5 ICMP'
+                # port scanning attack
+                elif attackType_list[j] == 'PORTSCAN':
+                    attackString = '(portscan)'
+                # detect all attacks on destination
+                else:
+                    attackString = '-> 10.10.1.5'
+
+                # check for attack defined in attackString
+                i=0
+                while i < len(alerts):
+                    line = alerts[i]
+
+                    if attackString in line:
+                        attackerIP = line.split(' ')[0]
+                        if attackerIP not in attacker_list:
+                            attacker_list.append(attackerIP)
+
+                    i = i+1
+                j = j+1
+
+            print 'Following IP addresses are being blocked:'
+            print '\n'.join(attacker_list)
+
+            # write list to attacker list
+            attackerFile = open('/tmp/attacker.txt','w')
+            attackerFile.write('\n'.join(attacker_list))
+            attackerFile.close()
+
+            # check if the file has changed and if it has changed, do the same
+            while True:
+                snort_alert_f_new = open(inputFile, 'r')
+                snort_alert_new = snort_alert_f_new.read()
+                snort_alert_f_new.close()
+
+                # if the file has not changed, then keep reading it
+                if snort_alert_new == snort_alert:
+                    # file not changed, pause before making next query
+                    time.sleep(pauseTime)
+                    continue
+                else:
+                    # file changed, change attacker file
+                    snort_alert = snort_alert_new
+                    break
+
+        except:
+            print 'Error Accessing file, running it again!'
+            attacker_list = []
+
+
+if __name__ == "__main__":
+    # System argument is the name of the snort alert file.
+    main()
+
---
+++ Control/PI_PID_controller/PID_controller4VNF.py
@@ -0,0 +1,120 @@
+__author__ = 'Nabeel'
+import time, sys
+
+
+def main(args):
+
+    path = args[1].strip(' \n').split(':')
+
+    file1 = open(path[0], 'r')
+    file2 = open(path[1], 'r')
+    file3 = open(path[2], 'r')
+    NFV1 = file1.read()
+    NFV2 = file2.read()
+    NFV3 = file3.read()
+
+    NFV1_old = NFV1
+    NFV2_old = NFV2
+    NFV3_old = NFV3
+
+
+    T = 50.0     # target load
+    X_t_1 = {'VNF1': 0.0, 'VNF2': 0.0, 'VNF3': 0.0}  # load on VNF at t-1 (past)
+    X_t = {'VNF1': 0.0, 'VNF2': 0.0, 'VNF3': 0.0}  # load on VNF at t (now)
+    Ki = 0.1     # how fast to change
+    Kd = 0.1     # K for D
+    L_t_1 = {'VNF1': 0.0, 'VNF2': 0.0, 'VNF3': 0.0}  # load on VNF at t-1
+    L_t = {'VNF1': 0.0, 'VNF2': 0.0, 'VNF3': 0.0}  # load on VNF at t (now)
+
+    while(True):
+
+        time.sleep(1.5)
+
+        # that means files have changed
+        if NFV1_old != NFV1 and NFV2_old != NFV2 and NFV3_old != NFV3:
+            try:
+                NFV1_latest = float(NFV1.split("=")[-1])
+                NFV2_latest = float(NFV2.split("=")[-1])
+                NFV3_latest = float(NFV3.split("=")[-1])
+
+                NFV1_old = (NFV1_old.split("=")[-1])
+                NFV2_old = (NFV2_old.split("=")[-1])
+                NFV3_old = (NFV3_old.split("=")[-1])
+
+            except:
+                print "Error is converting values!!!"
+                NFV1_old = NFV1
+                NFV2_old = NFV2
+                NFV3_old = NFV3
+                file1 = open(path[0], 'r')
+                file2 = open(path[1], 'r')
+                file3 = open(path[2], 'r')
+                NFV1 = file1.read()
+                NFV2 = file2.read()
+                NFV3 = file3.read()
+                continue;
+
+            prev = {'VNF1':0.0,'VNF2':0.0,'VNF3':0.0}
+
+            for i in range(len(path)):
+
+                if i == 0:
+                    nam = 'VNF'+str(i+1)
+                    L_t[nam] = NFV1_latest
+                    X_t[nam] = max(0, min(1, X_t_1[nam] + Ki * (L_t[nam] - T) / T + Kd * (((L_t[nam] - T) / T) - ((L_t_1[nam] - T) / T))))
+                    # updates things
+                    NFV1_old = NFV1
+                    prev[nam] = X_t_1[nam]
+                    X_t_1[nam] = X_t[nam]
+                    L_t_1[nam] = L_t[nam]
+                elif i == 1:
+                    nam = 'VNF' + str(i+1)
+                    L_t[nam] = NFV2_latest
+                    X_t[nam] = max(0, min(1, X_t_1[nam] + Ki * (L_t[nam] - T) / T + Kd * (((L_t[nam] - T) / T) - ((L_t_1[nam] - T) / T))))
+                    # updates things
+                    NFV2_old = NFV2
+                    prev[nam] = X_t_1[nam]
+                    X_t_1[nam] = X_t[nam]
+                    L_t_1[nam] = L_t[nam]
+                elif i == 2:
+                    nam = 'VNF' + str(i+1)
+                    L_t[nam] = NFV3_latest
+                    X_t[nam] = max(0, min(1, X_t_1[nam] + Ki * (L_t[nam] - T) / T + Kd * (((L_t[nam] - T) / T) - ((L_t_1[nam] - T) / T))))
+                    # updates things
+                    NFV3_old = NFV3
+                    prev[nam] = X_t_1[nam]
+                    X_t_1[nam] = X_t[nam]
+                    L_t_1[nam] = L_t[nam]
+                else:
+                    print 'ERROR IN UPDATING VALUES!!!'
+                    NFV1_old = NFV1
+                    NFV2_old = NFV2
+                    NFV3_old = NFV3
+                    file1 = open(path[0], 'r')
+                    file2 = open(path[1], 'r')
+                    file3 = open(path[2], 'r')
+                    NFV1 = file1.read()
+                    NFV2 = file2.read()
+                    NFV3 = file3.read()
+                    continue;
+
+            # get the last value for CPU usage
+            print "** PID CONTROLLER **"
+            print "Target CPU load at VNFs:            ", T
+            print "Current CPU load at VNF1, VNF2 and VNF3:           ", NFV1.split("=")[-1], ' : ',NFV2.split("=")[-1],' : ',NFV3.split("=")[-1]
+            print "% of flows to be send to VNF2, VNF3 and VNF4:      ", X_t['VNF1'] * 100, '%', ' : ',X_t['VNF2'] * 100, '%'' : ',X_t['VNF3'] * 100, '%'
+            print "% of flows previously send to VNF2, VNF3 and VNF4:: ", prev['VNF1'] * 100, '%', ' : ',prev['VNF2'] * 100, '%', ' : ',prev['VNF3'] * 100, '%'
+            print ""
+
+            f_out = open("NFV_ratio_4VNF.txt",'w')
+            f_out.write("X="+str(X_t['VNF1'])+':'+"X="+str(X_t['VNF2'])+':'+"X="+str(X_t['VNF3']))
+
+        file1 = open(path[0], 'r')
+        file2 = open(path[1], 'r')
+        file3 = open(path[2], 'r')
+        NFV1 = file1.read()
+        NFV2 = file2.read()
+        NFV3 = file3.read()
+
+if __name__ == "__main__":
+   main(sys.argv)
